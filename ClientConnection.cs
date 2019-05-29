9f948c2f0405093c29677e5e0c4de9e243e90a9c
Twist <twistedsprites@gmail.com>
2019-05-29 07:53:19 -0500

    Initial Upload

---
+++ ClientConnection.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class ClientConnection
+    {
+        Stream OutputStream;
+        HttpListenerResponse response;
+        public ClientConnection(HttpListenerContext context) {
+            HttpListenerRequest request = context.Request;
+            response = context.Response;
+            OutputStream = response.OutputStream;
+        }
+        public bool SendData(string data) {
+            try
+            {
+                byte[] buffer = System.Text.Encoding.UTF8.GetBytes(data);
+                response.ContentLength64 = buffer.Length;
+                OutputStream.Write(buffer, 0, buffer.Length);
+                OutputStream.Flush();
+                return true;
+            }
+            catch (Exception err) {
+                Console.WriteLine(err.Message);
+                return false;
+            }
+        }
+        public void CloseConnection() {
+            // You must close the output stream.
+            OutputStream.Close();
+            response.Close();
+        }
+    }
+}
---
+++ HttpServer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Threading;
+
+namespace ConsoleApp1
+{
+    public class HttpServer
+    {
+        public bool RunServer = true;
+        public List<ClientConnection> Connections = new List<ClientConnection>();
+        public void StartServer(string[] prefixes) {
+            Thread thread = new Thread(()=> {
+            
+            // Create a listener.
+            HttpListener listener = new HttpListener();
+            // Add the prefixes.
+            foreach (string s in prefixes)
+            {
+                listener.Prefixes.Add(s);
+            }
+            listener.Start();
+            Console.WriteLine("Listening...");
+            while (RunServer)
+            {
+                // Note: The GetContext method blocks while waiting for a request. 
+                HttpListenerContext context = listener.GetContext();
+                Connections.Add(new ClientConnection(context));
+            }
+            listener.Stop();
+            listener.Close();
+            });
+            thread.Start();
+
+        }
+
+    }
+}
